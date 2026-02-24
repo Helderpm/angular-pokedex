d79218a46730742815d464314350c5be2619561c
helderPM <hmartins123@hotmail.com>
2026-02-24 13:57:38 +0100

    fix erros img charge and add login page

---
+++ clear-storage.js
@@ -0,0 +1,4 @@
+// Clear localStorage and reload the page
+localStorage.removeItem('pokemons');
+console.log('Pokemon localStorage cleared. Reloading page...');
+location.reload();
---
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,53 @@
+import { DatePipe } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { PokemonBorderDirective } from '../../pokemon-border.directive';
+import { PokemonService } from '../../pokemon.service';
+import { Pokemon } from '../../pokemon.model';
+import { RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+@Component({
+  selector: 'app-pokemon-list',
+  standalone: true,
+  imports: [DatePipe, RouterLink, PokemonBorderDirective],
+  templateUrl: './pokemon-list.component.html',
+  styles: [
+    `
+      .pokemon-card {
+        cursor: pointer;
+      }
+    `,
+  ],
+})
+export class PokemonListComponent {
+  readonly pokemonService = inject(PokemonService);
+  readonly pokemonList = toSignal(this.pokemonService.getPokemonList());
+  readonly loading = computed(() => !this.pokemonList());
+  readonly searchTerm = signal('');
+  readonly resetting = signal(false);
+  readonly pokemonListFiltered = computed(() => {
+    return this.pokemonList()?.filter((pokemon) =>
+      pokemon.name
+        .toLowerCase()
+        .includes(this.searchTerm().trim().toLowerCase()),
+    );
+  });
+  
+  size(pokemon: Pokemon): string {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+    if (pokemon.life >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  }
+
+  resetPokemonList(): void {
+    this.resetting.set(true);
+    this.pokemonService.resetPokemonList().subscribe(() => {
+      this.resetting.set(false);
+      window.location.reload();
+    });
+  }
+}
